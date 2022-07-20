472935433505db8a77e3b9aeada417f6a3652f0b
Gorun Milic <73401771+Gorun-Milic@users.noreply.github.com>
2022-07-20 21:44:52 +0200

    Update SearchRequest.js

--- src/js/model/SearchRequest.js
+++ src/js/model/SearchRequest.js
@@ -1,13 +1,12 @@
 export default class SearchRequest {
 
-    API_KEY = '8217b56d767b6ba420281b7b6db7cfc0';
+    API_KEY = 'secret key';
 
     constructor() {
     }
 
     //vraca prognozu na osnovu lat i lon async drugi nacin
     async getWeatherData(lat, lon) {
-        let api = '8217b56d767b6ba420281b7b6db7cfc0';
         const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${this.API_KEY}`);
         const weather = await response.json();
         return weather;
@@ -40,7 +39,6 @@ export default class SearchRequest {
 
     //vraca naziv grada na osnovu lat i lon
     async getNearestCity(lat, lon) {
-        let api = '8217b56d767b6ba420281b7b6db7cfc0';
         const response = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${1}&appid=${this.API_KEY}`);
         const city = await response.json();
         return city[0].name;
